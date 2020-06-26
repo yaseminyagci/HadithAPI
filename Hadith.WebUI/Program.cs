d055ba3668b263c9f0c9c4c2d98cb321ef4b8d9f
Yasemin <y.yagci@metacortex.com.tr>
2020-06-26 15:10:29 +0300

    en son hal

--- Hadith.WebUI/Program.cs
+++ Hadith.WebUI/Program.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Hadith.WebUI
 {
@@ -13,6 +15,7 @@ namespace Hadith.WebUI
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration().BuildLoggerConfiguration();
             CreateHostBuilder(args).Build().Run();
         }
 
---
+++ Logging/BuildLog.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.RollingFileAlternate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logging
+{
+    public static class BuildLog
+    {
+        public static ILogger BuildLoggerConfiguration(this LoggerConfiguration loggerConfiguration) => loggerConfiguration.MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Migrations", LogEventLevel.Debug)
+                .Enrich.FromLogContext()
+                .Enrich.WithUserName()
+                .Enrich.WithExceptionDetails()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5341") // <- Added
+                .WriteTo.RollingFileAlternate(new RenderedCompactJsonFormatter(), "../Logging/Logs", fileSizeLimitBytes: 314572800) //300mb
+                .CreateLogger();
+    }
+}
